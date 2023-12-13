3f6b68ec1a453418553e6f5b11d7dc7ac6679c94
Borball Zhai <borball.zh@gmail.com>
2023-12-13 15:52:14 -0500

    reformat

--- irq/irqbalance-fix.sh
+++ irq/irqbalance-fix.sh
@@ -6,6 +6,7 @@ if [[ -a $cpu_manager_state_file ]]; then
   cat $cpu_manager_state_file |jq
   cpu_cores=$(cat $cpu_manager_state_file|jq -r '.. |.baseband? |select(. != null)')
   echo "baseband cpu cores: $cpu_cores"
+
   if [[ -n $cpu_cores ]]; then
     cpu_list=($(echo "$cpu_cores" | tr , '\n'|awk -F - 'NF==2{for(i=$1;i<=$2;i++) print i} NF==1{print $1}'| paste -d " " -s -))
     cpu_mask=0
@@ -13,15 +14,19 @@ if [[ -a $cpu_manager_state_file ]]; then
       cpu_bit=$((1 << $cpu))
       cpu_mask=$(($cpu_mask | $cpu_bit))
     done
+
     banned_cpus_mask=$(printf '%016llx\n' "$cpu_mask" |sed -r -e 's/^.{8}/&,/')
-    current_banned_cpus_str=$(grep ^IRQBALANCE_BANNED_CPUS /host/etc/sysconfig/irqbalance)
     update_banned_cpus_str="IRQBALANCE_BANNED_CPUS=\"$banned_cpus_mask\""
+
+    current_banned_cpus_str=$(grep ^IRQBALANCE_BANNED_CPUS /host/etc/sysconfig/irqbalance)
     echo "current banned cpus: $current_banned_cpus_str"
+
     if [[ "$current_banned_cpus_str" = "$update_banned_cpus_str" ]]; then
       echo "no update in /etc/sysconfig/irqbalance"
     else
       echo "will update to $banned_cpus_mask in /etc/sysconfig/irqbalance"
       sed -i "s/^IRQBALANCE_BANNED_CPUS.*/$update_banned_cpus_str/" /host/etc/sysconfig/irqbalance
+
       echo "restart irqbalance.service"
       /host/sbin/chroot /host /usr/bin/systemctl restart irqbalance.service
     fi
