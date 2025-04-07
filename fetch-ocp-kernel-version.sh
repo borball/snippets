fe626bd9c8f00e0730195a9a6c67b9f42a507eaa
Borball Zhai <borball.zh@gmail.com>
2025-04-07 08:52:47 -0400

    Update fetch-ocp-kernel-version.sh

--- fetch-ocp-kernel-version.sh
+++ fetch-ocp-kernel-version.sh
@@ -1,8 +1,21 @@
+function ocp-kernel-version {
+  local ocp_version=$1
+  if [[ -z "${ocp_version}" ]]; then
+    echo "Usage: ${FUNCNAME} <ocp_version>"
+    return -1
+  fi
 
-for i in {25..44}; do
-  os_image=$(oc adm release info 4.14.${i} --image-for rhel-coreos-extensions)
-  mkdir -p /run/mco-machine-os-content/os-content/4.14.${i}
-  oc image extract --path /usr/share/rpm-ostree/extensions:/run/mco-machine-os-content/os-content/4.14.${i} --registry-config ~/pull-secret.json $os_image
-  ls /run/mco-machine-os-content/os-content/4.14.${i}/extensions/kernel-rt-core*
-  rm -rf /run/mco-machine-os-content/os-content/4.14.${i}
-done
+  pull_secret=${HOME}/cluster/pull-secret.json
+  if [[ ! -f ${pull_secret} ]]; then
+    echo "Pull secret (${pull_secret}) doesn't exists"
+    return -1
+  fi
+  os_image=$(oc adm release info ${ocp_version} --image-for rhel-coreos-extensions)
+  if [[ -z ${os_image} ]]; then
+    echo "Unable to determin os image for openshift version ${ocp_version}"
+    echo "Only works for release 4.14+"
+    return -1
+  fi
+  echo "Inspect ${ocp_version}/rhel-coreos-extensions ${os_image}"
+  oc image extract --registry-config ${pull_secret} $os_image --dry-run |grep kernel-rt-core
+}
